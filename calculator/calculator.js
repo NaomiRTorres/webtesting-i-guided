7856c373133494937cf08a44963fc152a647cde2
Naomi Ruemmely Torres <59476335+NaomiPriest97@users.noreply.github.com>
2020-04-22 13:49:43 -0400

    saving notes

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,26 @@ module.exports = {
   add,
 };
 
-function add() {
-  return null;
+// function add(...numbers) {
+//   //console.log(numbers);
+
+//   return numbers.reduce((sum, number) => {
+//     return sum + number;
+//   }, 0)
+  
+//   // let sum = 0;
+//   // for(var i = 0; i < numbers.length; i++){
+//   //   sum += numbers[i];
+//   // }
+//   // return Number(sum);
+// }
+
+function add(args){
+  const numbers = Array.isArray(args) ? args: Array.from(arguments);
+
+  //console.log('numbers:', numbers);
+
+  return numbers.reduce((sum, number) => {
+    return sum + number;
+  }, 0);
 }
--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,47 @@
 const { add } = require('./calculator.js');
 
 // test away!
+describe('calculator.js', () => {
+    test('should run tests without errors', () => {
+        //arrange -> setup the world
+        //act ----> execute the code
+        //assert -> check our assumptions
+        expect(true).toBe(true); //toBe is a matcher
+    })
+    describe('.add()', function(){
+        it('should add two numbers', function (){
+           expect(add(2, 2)).toBe(4);
+           expect(add(2, 1)).toBe(3);
+           expect(add(2, 0)).toBe(2);
+        });
+
+        it('should return 0 when called with no arguments', function(){
+            expect(add()).toBe(0);
+        });
+
+        it('should return the number when called with a single number', function(){
+            expect(add(3)).toBe(3);
+            expect(add(0)).toBe(0);
+            expect(add(-5)).toBe(-5);
+        })
+
+        it('should sum a list of numbers seperated by comma', function(){
+            expect(add(0, 2, 3)).toBe(5);
+            expect(add(1, 2, -5)).toBe(-2);
+        });
+
+        it('should support an array of numbers', function(){
+            expect(add([1, 2, -5])).toBe(-2);
+            expect(add([0, 2, 3])).toBe(5);
+        })
+
+    })
+})
+
+
+// RULES
+// try to make tests fail, before making them pass
+// jest runs files ending in .test.js or .spec.js
+// jest runs .js files contained inside any _tests_ folder at any level
+// low hanging fruit first (happy path -> if the world is perfect, what does it do)
+// you;ll most likely have to make it up and make it happen
